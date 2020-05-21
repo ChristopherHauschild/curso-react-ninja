4f2cea84f8a08b9f0f4bc178afdcf219db30b7ae
ChristopherHauschild <chrishaus01@gmail.com>
2020-05-21 15:32:19 -0300

    Updated

---
+++ M03/todo-list-15_31/src/redux-flow/reducers/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import { combineReducers } from 'redux'
+import todos from './todos'
+import visibilityFilter from './visibility-filter'
+
+// const rootReducer = (state = {}, action) => {
+//   return {
+//     todos: reducerTodos(state.todos, action),
+//     visibilityFilter: reducerVisibilityFilter(state.visibilityFilter, action)
+//   }
+// }
+
+export default combineReducers({
+  todos,
+  visibilityFilter
+})
